e8eae919934df9e469a270658236169fcd66fb31
Tao Sun <freesunnyday@gmail.com>
2018-10-26 11:35:15 +0200

    add comment; add dimension check

--- Tao‘s folder/best_ever/implementations.py
+++ Tao‘s folder/best_ever/implementations.py
@@ -30,21 +30,80 @@ def batch_iter(y, tx, batch_size, num_batches=1, shuffle=True):
             yield shuffled_y[start_index:end_index], shuffled_tx[start_index:end_index]
 
 
+def _check_dimension(y_true, y_pred):
+    """Check if the dimension of true target and predicted target match."""
+    # if 1D, reshape as 2D
+    if y_true.ndim == 1:
+        y_true = y_true.reshape((-1, 1))
+    if y_pred.ndim == 1:
+        y_pred = y_pred.reshape((-1, 1))
+
+    # check the number of samples
+    if y_true.shape[0] != y_pred.shape[0]:
+        raise ValueError("Number of samples does not correspond: ",
+                         "y_true has {}, y_pred has {}.".format(y_true.shape[0], y_pred.shape[0]))
+
+    # check the number of output
+    if y_true.shape[1] != y_pred.shape[1]:
+        raise ValueError("Number of output does not correspond: ",
+                         "y_true has {}, y_pred has {}.".format(y_true.shape[1], y_pred.shape[1]))
+
+    return y_true, y_pred
+
+
 def compute_mse_loss(y, tx, w):
     """Calculate mse loss."""
-    e = y - tx.dot(w)
-    return 1/2 * np.mean(e**2)
+    pred = tx.dot(w)
+    y, pred = _check_dimension(y, pred)
+    err = y - pred
+    return np.mean(0.5 * err ** 2)
 
 
 def compute_gradient_linear(y, tx, w):
     """Compute the gradient of linear regression."""
-    err = y - tx.dot(w)
+    pred = tx.dot(w)
+    y, pred = _check_dimension(y, pred)
+    err = y - pred
     grad = -tx.T.dot(err) / len(err)
     return grad
 
 
 def least_squares_GD(y, tx, initial_w, max_iters, gamma):
-    """Linear regression using gradient descent."""
+    """Linear regression using gradient descent.
+
+    Parameters
+    ----------
+    y : Target
+    tx : Data
+    initial_w : Initial weight vector
+    max_iters : Number of iterations
+    gamma : Learning rate or step size in each iteration
+
+    Returns
+    -------
+    w : Weight vector after final iteration
+    loss : Corresponding loss calculated by mean square error
+    """
+    n_samples, n_features = tx.shape
+
+    # check target
+    if y.ndim > 2:
+        raise ValueError("Target y has wrong shape: {}.".format(y.shape))
+    if y.ndim == 1:
+        y = y.reshape((-1, 1))
+    if y.shape[0] != n_samples:
+        raise ValueError("Number of samples in tx and y does not correspond: ",
+                         "tx has {}, y has {}.".format(n_samples, y_shape[0]))
+
+    n_output = y.shape[1]
+
+    # check initial weight
+    if initial_w.ndim == 1:
+        initial_w = initial_w.reshape((-1, 1))
+    if initial_w.shape != (n_features, n_output):
+        raise ValueError("Initial weight has wrong shape: ",
+                         "{} != {}.".format(initial_w.shape, (n_features, n_output)))
+
     # initialize weight
     w = initial_w
 
@@ -61,7 +120,41 @@ def least_squares_GD(y, tx, initial_w, max_iters, gamma):
 
 
 def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
-    """Linear regression using stochastic gradient descent."""
+    """Linear regression using stochastic gradient descent.
+
+    Parameters
+    ----------
+    y : Target
+    tx : Data
+    initial_w : Initial weight vector
+    max_iters : Number of iterations
+    gamma : Learning rate or step size in each iteration
+
+    Returns
+    -------
+    w : Weight vector after final iteration
+    loss : Corresponding loss calculated by mean square error
+    """
+    n_samples, n_features = tx.shape
+
+    # check target
+    if y.ndim > 2:
+        raise ValueError("Target y has wrong shape: {}.".format(y.shape))
+    if y.ndim == 1:
+        y = y.reshape((-1, 1))
+    if y.shape[0] != n_samples:
+        raise ValueError("Number of samples in tx and y does not correspond: ",
+                         "tx has {}, y has {}.".format(n_samples, y_shape[0]))
+
+    n_output = y.shape[1]
+
+    # check initial weight
+    if initial_w.ndim == 1:
+        initial_w = initial_w.reshape((-1, 1))
+    if initial_w.shape != (n_features, n_output):
+        raise ValueError("Initial weight has wrong shape: ",
+                         "{} != {}.".format(initial_w.shape, (n_features, n_output)))
+
     # initialize weight
     w = initial_w
 
@@ -79,7 +172,18 @@ def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
 
 
 def least_squares(y, tx):
-    """Least squares regression using normal equations."""
+    """Least squares regression using normal equations.
+
+    Parameters
+    ----------
+    y : Target
+    tx : Data
+
+    Returns
+    -------
+    w : Weight vector
+    loss : Corresponding loss calculated by mean square error
+    """
     a = tx.T.dot(tx)
     b = tx.T.dot(y)
     w = np.linalg.solve(a, b)
@@ -88,8 +192,20 @@ def least_squares(y, tx):
 
 
 def ridge_regression(y, tx, lambda_):
-    """Ridge regression using normal equations."""
-    aI = 2 * tx.shape[0] * lambda_ * np.identity(tx.shape[1])
+    """Ridge regression using normal equations.
+
+    Parameters
+    ----------
+    y : Target
+    tx : Data
+    lambda_ : Regularization parameter
+
+    Returns
+    -------
+    w : Weight vector
+    loss : Corresponding loss calculated by mean square error
+    """
+    aI = 2 * tx.shape[0] * lambda_ * np.eye(tx.shape[1])
     a = tx.T.dot(tx) + aI
     b = tx.T.dot(y)
     w = np.linalg.solve(a, b)
@@ -109,6 +225,7 @@ def compute_nl_loss(y, tx, w):
     epsilon = 1e-15
     pred = sigmoid(tx.dot(w))
     pred = np.clip(pred, epsilon, 1-epsilon)
+    y, pred = _check_dimension(y, pred)
 
     loss = y.T.dot(np.log(pred)) + (1 - y).T.dot(np.log(1 - pred))
 
@@ -118,12 +235,47 @@ def compute_nl_loss(y, tx, w):
 def compute_gradient_logistic(y, tx, w):
     """Compute the gradient of logistic regression."""
     pred = sigmoid(tx.dot(w))
+    y, pred = _check_dimension(y, pred)
     grad = tx.T.dot(pred - y)
     return grad
 
 
 def logistic_regression(y, tx, initial_w, max_iters, gamma):
-    """Logistic regression using SGD."""
+    """Logistic regression using SGD.
+
+    Parameters
+    ----------
+    y : Target
+    tx : Data
+    initial_w : Initial weight vector
+    max_iters : Number of iterations
+    gamma : Learning rate or step size in each iteration
+
+    Returns
+    -------
+    w : Weight vector after final iteration
+    loss : Corresponding loss calculated by negative log likelihood
+    """
+    n_samples, n_features = tx.shape
+
+    # check target
+    if y.ndim > 2:
+        raise ValueError("Target y has wrong shape: {}.".format(y.shape))
+    if y.ndim == 1:
+        y = y.reshape((-1, 1))
+    if y.shape[0] != n_samples:
+        raise ValueError("Number of samples in tx and y does not correspond: ",
+                         "tx has {}, y has {}.".format(n_samples, y_shape[0]))
+
+    n_output = y.shape[1]
+
+    # check initial weight
+    if initial_w.ndim == 1:
+        initial_w = initial_w.reshape((-1, 1))
+    if initial_w.shape != (n_features, n_output):
+        raise ValueError("Initial weight has wrong shape: ",
+                         "{} != {}.".format(initial_w.shape, (n_features, n_output)))
+
     # initialize weight
     w = initial_w
 
@@ -140,11 +292,6 @@ def logistic_regression(y, tx, initial_w, max_iters, gamma):
     return w, loss
 
 
-def compute_nl_loss_regularization(y, tx, w, lambda_):
-    """Compute the loss by negative log likelihood and l2 regularization."""
-    return compute_nl_loss(y, tx, w) + lambda_ * np.squeeze(w.T.dot(w))
-
-
 def compute_gradient_reg_logistic(y, tx, w, lambda_):
     """Compute the gradient of regularized logistic regression."""
     w_reg = w
@@ -153,7 +300,42 @@ def compute_gradient_reg_logistic(y, tx, w, lambda_):
 
 
 def reg_logistic_regression(y, tx, lambda_, initial_w, max_iters, gamma):
-    """Regularized logistic regression using SGD."""
+    """Regularized logistic regression using SGD.
+
+    Parameters
+    ----------
+    y : Target
+    tx : Data
+    lambda_ : Regularization parameter
+    initial_w : Initial weight vector
+    max_iters : Number of iterations
+    gamma : Learning rate or step size in each iteration
+
+    Returns
+    -------
+    w : Weight vector after final iteration
+    loss : Corresponding loss calculated by negative log likelihood
+    """
+    n_samples, n_features = tx.shape
+
+    # check target
+    if y.ndim > 2:
+        raise ValueError("Target y has wrong shape: {}.".format(y.shape))
+    if y.ndim == 1:
+        y = y.reshape((-1, 1))
+    if y.shape[0] != n_samples:
+        raise ValueError("Number of samples in tx and y does not correspond: ",
+                         "tx has {}, y has {}.".format(n_samples, y_shape[0]))
+
+    n_output = y.shape[1]
+
+    # check initial weight
+    if initial_w.ndim == 1:
+        initial_w = initial_w.reshape((-1, 1))
+    if initial_w.shape != (n_features, n_output):
+        raise ValueError("Initial weight has wrong shape: ",
+                         "{} != {}.".format(initial_w.shape, (n_features, n_output)))
+
     # initialize weight
     w = initial_w
 
@@ -165,6 +347,6 @@ def reg_logistic_regression(y, tx, lambda_, initial_w, max_iters, gamma):
             w = w - gamma * grad
 
     # calculate loss with l2 regularization
-    loss = compute_nl_loss_regularization(y, tx, w, lambda_)
+    loss = compute_nl_loss(y, tx, lambda_)
 
     return w, loss
